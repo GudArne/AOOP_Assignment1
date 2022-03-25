88a4b42433f1a7615fddd0d65ef14bdbf15dd72f
Mattias <mattias9922@hotmail.com>
2022-03-25 13:40:21 +0100

    RGB Task15 done

---
+++ Task15/RGB.java
@@ -0,0 +1,104 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class RGB extends JFrame implements ActionListener
+{
+    JButton redBtn;
+    JButton greenBtn;
+    JButton blueBtn;
+    JFrame window;
+    JPanel panel;
+    boolean red;
+    boolean blue = true;
+    boolean green;
+    int firstTime = 0;
+    Graphics2D g2;
+
+   public RGB()
+   {
+        this.window = new JFrame();
+
+        this.redBtn = new JButton("RED");
+        redBtn.addActionListener(this);
+        
+
+        this.blueBtn = new JButton("BLUE");
+        blueBtn.addActionListener(this);
+
+        this.greenBtn = new JButton("GREEN");
+        greenBtn.addActionListener(this);
+
+        this.panel = new JPanel()
+        {
+            @Override
+            public void paintComponent(Graphics g)
+            {
+                super.paintComponent(g);
+                 g2= (Graphics2D)g;
+                if(firstTime == 0){
+                    g2.setColor(Color.RED);
+                    g2.fillOval(200, 200, 50, 50);
+                    firstTime = 1;
+                }
+                if(red == true){
+                    g2.setColor(Color.RED);
+                    g2.fillOval(200, 200, 50, 50);
+    
+                }
+                if(blue == true){
+                    g2.setColor(Color.BLUE);
+                    g2.fillOval(200, 200, 50, 50);
+                }
+                if(green == true){
+                    g2.setColor(Color.GREEN);
+                    g2.fillOval(200, 200, 50, 50);
+                }
+            }
+        };
+        panel.add(blueBtn);
+        panel.add(greenBtn);
+        panel.add(redBtn);
+
+        window.setSize(400,400);
+        window.add(panel);
+
+        window.pack();
+        window.setVisible(true);
+   }
+
+@Override
+public void actionPerformed(ActionEvent e) 
+{
+    if(e.getSource() == redBtn)
+    {
+        red = true;
+        blue = false;
+        green = false;
+        panel.repaint();
+    } 
+    if(e.getSource() == blueBtn)
+    {
+        blue = true;
+        red = false;
+        green = false;
+        panel.repaint();
+    } 
+    if(e.getSource() == greenBtn)
+    {
+        green = true;
+        red = false;
+        blue = false;
+        panel.repaint();
+    } 
+}
+    
+
+
+
+public static void main(String[] args) {
+    RGB rgb = new RGB();
+}
+}
+
